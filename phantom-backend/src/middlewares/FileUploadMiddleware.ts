0c47dc14480620387e24b7af3fb2ae5eda05fb59
Anuj Anthwal <anujanthwal98765432@gmail.com>
2025-06-29 17:22:15 +0530

    im

--- phantom-backend/src/middlewares/FileUploadMiddleware.ts
+++ phantom-backend/src/middlewares/FileUploadMiddleware.ts
@@ -1,12 +1,15 @@
 import multer from 'multer';
 import path from 'path';
 import type { Request} from 'express';
-
+import fs from 'fs';
 // For destination and filename callback
 type Callback = (error: Error | null, destination: string) => void;
 
 const storage: multer.StorageEngine = multer.diskStorage({
     destination: (request:Request,file:Express.Multer.File,callback:Callback)=>{
+        if(!fs.existsSync(String(process.env.STORAGE_PATH))){
+            fs.mkdirSync(String(process.env.STORAGE_PATH), { recursive: true });
+        }
         callback(null, path.join(String(process.env.STORAGE_PATH)));
     },
     filename: (request:Request,file:Express.Multer.File,callback:Callback)=>{
